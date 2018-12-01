95e9f8ab26ce6f3debb06ad093dbbf6b58beb1ce
cmput274 <cmput274@ualberta.ca>
2018-12-01 12:43:33 -0700

    here's the utilities file that you wanted:)

---
+++ songs.h
@@ -0,0 +1,19 @@
+/** Description: This file will create serveral arrays that hold the beats and the frquencies of certain songs to be played 
+*by the buzzer. It will also have 2 structs that will hold these arrays
+*/
+#include <Arduino.h>
+
+void setup()
+{
+    Serial.begin(9600);
+    char key;
+}
+
+int main()
+{
+    float freq1[] = {}
+}
+//arrays 
+//freq array 
+//beat array 
+//struct with the two arrays 
